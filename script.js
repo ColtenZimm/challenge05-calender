39c18f490013c9124c87998fd8fd97963489b7ab
ColtenZimm <coltenz00@gmail.com>
2023-04-20 18:46:56 -0500

    added timeblocks and color

--- script.js
+++ script.js
@@ -103,3 +103,102 @@ function createHourBlock(hour) {
   return hourBlock;
 }
 
+var saveButtons = $('.saveBtn')
+var today = dayjs();
+$('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm A'))
+
+function setBackgroundColor (time) {
+  //present conditions
+  if (time===9) {
+    $('#hour-9').attr('style','background-color: #ff6961;')
+  } 
+  if (time===10) {
+    $('#hour-10').attr('style','background-color: #ff6961;')
+  } 
+  if (time===11) {
+    $('#hour-11').attr('style','background-color: #ff6961;')
+  } 
+  if (time===12) {
+    $('#hour-12').attr('style','background-color: #ff6961;')
+  } 
+  if (time===13) {
+    $('#hour-1').attr('style','background-color: #ff6961;')
+  } 
+  if (time===14) {
+    $('#hour-2').attr('style','background-color: #ff6961;')
+  } 
+  if (time===15) {
+    $('#hour-3').attr('style','background-color: #ff6961;')
+  } 
+  if (time===16) {
+    $('#hour-4').attr('style','background-color: #ff6961;')
+  } 
+  if (time===17) {
+    $('#hour-5').attr('style','background-color: #ff6961;')
+  }
+
+  //past conditions
+  if (time>=10) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+  } 
+  if (time>=11) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+  } 
+  if (time>=12) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+  } 
+  if (time>=13) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+    $('#hour-12').attr('style','background-color: #d3d3d3;')
+  }
+  if (time>=14) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+    $('#hour-12').attr('style','background-color: #d3d3d3;')
+    $('#hour-1').attr('style','background-color: #d3d3d3;')
+  }
+  if (time>=15) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+    $('#hour-12').attr('style','background-color: #d3d3d3;')
+    $('#hour-1').attr('style','background-color: #d3d3d3;')
+    $('#hour-2').attr('style','background-color: #d3d3d3;')
+  } 
+  if (time>=16) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+    $('#hour-12').attr('style','background-color: #d3d3d3;')
+    $('#hour-1').attr('style','background-color: #d3d3d3;')
+    $('#hour-2').attr('style','background-color: #d3d3d3;')
+    $('#hour-3').attr('style','background-color: #d3d3d3;')
+  } 
+  if (time>=17) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+    $('#hour-12').attr('style','background-color: #d3d3d3;')
+    $('#hour-1').attr('style','background-color: #d3d3d3;')
+    $('#hour-2').attr('style','background-color: #d3d3d3;')
+    $('#hour-3').attr('style','background-color: #d3d3d3;')
+    $('#hour-4').attr('style','background-color: #d3d3d3;')
+  } 
+  if (time>18) {
+    $('#hour-9').attr('style','background-color: #d3d3d3;')
+    $('#hour-10').attr('style','background-color: #d3d3d3;')
+    $('#hour-11').attr('style','background-color: #d3d3d3;')
+    $('#hour-12').attr('style','background-color: #d3d3d3;')
+    $('#hour-1').attr('style','background-color: #d3d3d3;')
+    $('#hour-2').attr('style','background-color: #d3d3d3;')
+    $('#hour-3').attr('style','background-color: #d3d3d3;')
+    $('#hour-4').attr('style','background-color: #d3d3d3;')
+    $('#hour-5').attr('style','background-color: #d3d3d3;')
+  } 
+}
